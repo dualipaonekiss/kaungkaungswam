3aed591f6711ddf22368a4a6e5ba0cca9b8a75ca
Tun-Min <tun2min@cloudsource.co.jp>
2023-02-09 15:53:14 +0630

    emailupdate

---
+++ kaungkaung2.py
@@ -0,0 +1,22 @@
+import smtplib
+
+server =smtplib.SMTP("smtp.gmail.com",587)
+server.starttls()
+
+server.login("kaungkaungswanhtet@cloudsource.co.jp","jahvisqwfvsscsrr")
+
+server.sendmail("kaungkaungswanhtet@cloudsource.co.jp","tharhtet.3@cloudsource.co.jp","hello nay kaung lar ser pe pe lar")
+
+
+
+     
+
+
+
+
+
+
+
+
+        
+        
